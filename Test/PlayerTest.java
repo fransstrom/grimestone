212bb1a7908af50d940972163df51ea42aa39a89
Frans Herrstrom <frans.herrstrom@gmail.com>
2018-10-29 10:47:06 +0100

    CreatureCard setActive/isActive test

--- Test/PlayerTest.java
+++ Test/PlayerTest.java
@@ -7,6 +7,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PlayerTest {
