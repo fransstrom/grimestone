6d78fa14eb67d0e6230b453ff17403118eefed36
Milan Stoiljkovic <misto1977@yahoo.com>
2018-10-29 10:36:44 +0100

    Additional tests with Mockito

--- Test/PlayerTest.java
+++ Test/PlayerTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
@@ -37,9 +38,10 @@ class PlayerTest {
 
     @Test
     void deckIsEmpty() {
-        assertTrue( player1.deckIsEmpty());
-        
-
+        Mockito.when(deckMock.isEmpty()).thenReturn( false );
+        assertFalse( player1.deckIsEmpty());
+        Mockito.when( deckMock.isEmpty() ).thenReturn( true );
+        assertTrue( player1.deckIsEmpty() );
     }
 
     @Test
