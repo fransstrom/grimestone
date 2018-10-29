c5d4dd245fb21e880b88f6f114b063bbe41b1ee8
Mikael Andersson <mickepunktjohn@gmail.com>
2018-10-29 14:59:28 +0100

    Added verify method call

--- Test/GUITest.java
+++ Test/GUITest.java
@@ -1,12 +1,23 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class GUITest {
+
+    GUI gui;
+
+    @Mock
+    GameEngine gameEngine;
 
     @BeforeEach
     void setUp() {
+        gui = new GUI(gameEngine);
     }
 
     @Test
@@ -15,6 +26,9 @@ class GUITest {
 
     @Test
     void printPlayerHP() {
+        gui.printPlayerHP();
+        Mockito.verify(gameEngine, Mockito.times(1)).getPlayer1();
+        Mockito.verify(gameEngine, Mockito.times(1)).getPlayer2();
     }
 
     @Test
