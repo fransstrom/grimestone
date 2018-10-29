6ca0f2dd02abc76af712a218d26897f215d77f3e
Kevin W <kevinhaj@gmail.com>
2018-10-29 15:23:27 +0100

    Initial Tests

--- Test/BattleLogicTest.java
+++ Test/BattleLogicTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Spy;
 
 
@@ -8,7 +9,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class BattleLogicTest {
 
     @Spy
-    Card card = new CreatureCard(10);
+    CreatureCard card = new CreatureCard(10);
+
+    @Spy
+    CreatureCard card2 = new CreatureCard(20);
 
     @Spy
     Player player = new Player();
@@ -29,7 +33,10 @@ class BattleLogicTest {
 
     @Test
     void cardVsCard() {
-        //Kev
+        assertTrue(card.getHp()==10);
+        assertTrue(card2.getHp()==20);
+        battleLogic.cardVsCard();
+        assertTrue(card.getHp()<10 || card2.getHp()<20);
     }
 
     @Test
