4c3edd7fc994f06b810be119cf4ecf93a486078d
Marie-Charlotte Persson <MagikWytch@gmail.com>
2018-10-30 11:13:11 +0100

    Adding test

--- Test/GUITest.java
+++ Test/GUITest.java
@@ -6,6 +6,13 @@ import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class GUITest {
 
@@ -15,6 +22,8 @@ class GUITest {
     private GameEngine gameEngine;
     @Mock
     private Player mockPlayer;
+    @Mock
+    private CreatureCard creatureCard;
 
     @BeforeEach
     void setUp() {
@@ -27,16 +36,26 @@ class GUITest {
 
     @Test
     void printPlayerHP() {
-        Mockito.when(gameEngine.getPlayer1()).thenReturn(mockPlayer);
-        Mockito.when(gameEngine.getPlayer2()).thenReturn(mockPlayer);
-        Mockito.when(mockPlayer.getHp()).thenReturn(10);
+        when(gameEngine.getPlayer1()).thenReturn(mockPlayer);
+        when(gameEngine.getPlayer2()).thenReturn(mockPlayer);
+        when(mockPlayer.getHp()).thenReturn(10);
         gui.printPlayerHP();
-        Mockito.verify(gameEngine, Mockito.times(1)).getPlayer1();
-        Mockito.verify(gameEngine, Mockito.times(1)).getPlayer2();
+        verify(gameEngine, times(1)).getPlayer1();
+        verify(gameEngine, times(1)).getPlayer2();
     }
 
     @Test
     void printCardsInHand() {
+        ArrayList<Card> cardList = new ArrayList<>();
+        cardList.add(creatureCard);
+
+        when(gameEngine.getActivePlayer()).thenReturn(mockPlayer);
+        when(mockPlayer.getHand()).thenReturn(cardList);
+
+        gui.printCardsInHand();
+        verify(gameEngine, times(1)).getActivePlayer();
+        verify(mockPlayer, times(1)).getHand();
+
     }
 
     @Test
