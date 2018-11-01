65b09a5d26779b817bdd4d8636b00d66ff92a63b
Disa <dvalemark@gmail.com>
2018-11-01 12:06:45 +0100

    Created class InputProcessor and getInput method

---
+++ src/main/java/InputProcessor.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class InputProcessor {
+    private static Scanner scan = new Scanner(System.in);
+
+    public int getInput(){
+        return scan.nextInt();
+    }
+}
