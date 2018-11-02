91956cc76d1b1d2455a726502f776fad4ca8783d
Disa <dvalemark@gmail.com>
2018-11-02 11:06:46 +0100

    Started writing tests for gameloop switchcase phase 1

---
+++ Test/GameloopTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.Nested;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class GameLoopTest {
+
+    private GameLoop gameLoop;
+    @Mock
+    GameEngine mockGameEngine;
+    @Mock
+    InputProcessor mockInputProcessor;
+    @Mock
+    ArrayList<Card> mockList;
+
+
+    @BeforeAll
+    void setUp(){
+        gameLoop = new GameLoop(mockGameEngine, mockInputProcessor);
+    }
+
+    @Nested
+    @DisplayName("Game loop phase 1")
+    class TestPhase1{
+
+        @BeforeAll
+        void setUp2(){
+            mockList = new ArrayList<>();
+        }
+
+        @Test
+        void pickCardFromHandToPutOnTable() {
+            when(mockInputProcessor.getInputInt()).thenReturn(1);
+            doNothing().when(mockGameEngine).pickCard(any(ArrayList.class));
+            verify(mockGameEngine, times(1)).pickCard(any(ArrayList.class));
+
+        }
+
+        @Test
+        void skip(){
+            when(mockInputProcessor.getInputInt()).thenReturn(2);
+
+        }
+
+    }
+
+
+}
---
+++ src/main/java/GameLoop.java
@@ -0,0 +1,17 @@
+public class GameLoop {
+  private   GameEngine gameEngine;
+  private   InputProcessor inputProcessor;
+
+    public GameLoop(GameEngine gameEngine, InputProcessor inputProcessor){
+       this.gameEngine = gameEngine;
+       this.inputProcessor = inputProcessor;
+    }
+
+    public void gameLoopPhase1() {
+
+    }
+
+    public void gameLoopPhase2(){
+
+    }
+}
