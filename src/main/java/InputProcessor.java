3fe5c2ee3c9d4dc9dea598743b5767061c61c766
Mikael Andersson <mickepunktjohn@gmail.com>
2018-11-05 19:11:20 +0100

    delete InputProcessor

--- src/main/java/InputProcessor.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class InputProcessor {
-    private static Scanner scan = new Scanner(System.in);
-
-    public int getInputInt(){
-        return scan.nextInt();
-    }
-}
