cc755b51bf057c5b010f229107d2d51c0a5a39d8
Mikael Andersson <mickepunktjohn@gmail.com>
2018-11-12 14:46:52 +0100

    Added class variables to card and provided constructors. Enum Type in Card class

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,7 +1,30 @@
 import java.util.UUID;
 
 public abstract class Card {
-    public Card() {
 
+    public enum Type{
+        WATER, FIRE, GRASS, GHOST
+    }
+
+    private String name;
+    private int manaCost;
+    private Type type;
+
+    public Card(String name, int manaCost, Type type) {
+        this.name = name;
+        this.manaCost = manaCost;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getManaCost() {
+        return manaCost;
+    }
+
+    public Type getType() {
+        return type;
     }
 }
--- src/main/java/CreatureCard.java
+++ src/main/java/CreatureCard.java
@@ -3,15 +3,16 @@ public class CreatureCard extends Card {
     private boolean active;
     private int hp;
 
-    public CreatureCard(){
-        super();
+    //If HP is not provided, a random value will be generated
+    public CreatureCard(String name, int manaCost, Type type){
+        super(name, manaCost, type);
         //Random HP from 1-7
         this.hp = RandomNumberGenerator.roll();
         this.active = false;
     }
 
-    public CreatureCard(int hp) {
-        super();
+    public CreatureCard(String name, int manaCost, Type type, int hp) {
+        super(name, manaCost, type);
         this.hp = hp;
         this.active = false;
     }
