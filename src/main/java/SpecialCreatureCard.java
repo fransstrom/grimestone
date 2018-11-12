df956a8d81ca81e87d1d6c1ef688eb0d19a9b278
Disa <dvalemark@gmail.com>
2018-11-12 10:33:54 +0100

    Added properties, constructor and getText.

--- src/main/java/SpecialCreatureCard.java
+++ src/main/java/SpecialCreatureCard.java
@@ -1,2 +1,13 @@
 public class SpecialCreatureCard extends CreatureCard {
+    IEffect effect;
+    String effectText;
+
+    SpecialCreatureCard(IEffect effect){
+        super();
+        this.effect = effect;
+    }
+
+    String getText(){
+        return this.effect.getText();
+    }
 }
