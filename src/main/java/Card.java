a3caddb4c841340a8286e0ca646bc34a16bd95f9
Mikael Andersson <mickepunktjohn@gmail.com>
2018-11-12 15:07:14 +0100

    Added logic and get/set

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -10,13 +10,27 @@ public abstract class Card {
     private int manaCost;
     private Type type;
 
+    public Card(){
+        this.name = "";
+        this.manaCost = 1;
+        this.type = Type.NEUTRAL;
+    }
+
     public Card(String name, int manaCost, Type type) {
         this.name = name;
         this.manaCost = manaCost;
         this.type = type;
     }
 
-    public boolean isSuperEffective(Type type){
+    public boolean isSuperEffective(Type opponentType){
+        switch (this.type){
+            case FIRE:
+                return opponentType == Type.GRASS;
+            case GRASS:
+                return opponentType == Type.WATER;
+            case WATER:
+                return opponentType == Type.FIRE;
+        }
         return false;
     }
 
@@ -30,5 +44,17 @@ public abstract class Card {
 
     public Type getType() {
         return type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setManaCost(int manaCost) {
+        this.manaCost = manaCost;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
     }
 }
