0d0681476e6890bddaaa4c481edc5264eda56e5c
Disa <dvalemark@gmail.com>
2018-11-12 10:44:29 +0100

    Added properties, constructor and getText

--- src/main/java/MagicCard.java
+++ src/main/java/MagicCard.java
@@ -1,2 +1,14 @@
 public class MagicCard extends Card {
+    IEffect effect;
+    String effectText;
+    boolean isConsumed;
+
+    public MagicCard (IEffect effect){
+        super();
+        this.effect =effect;
+    }
+
+    String getText(){
+        return this.effect.getText();
+    }
 }
