98ee4c9c047db0994eb06d8c4fc25aa4e1730db8
JonasFisk <Ldr_shadow@hotmail.com>
2018-11-16 01:54:40 +0100

    Added connection method for mysql

--- src/main/java/HighScoreDB.java
+++ src/main/java/HighScoreDB.java
@@ -1,2 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class HighScoreDB {
+
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("JDBC:mysql://localhost:3306/players", "grimestone", "abc123");
+    }
+
 }
