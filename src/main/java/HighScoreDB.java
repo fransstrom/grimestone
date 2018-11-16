51c9a2abdc66d4b1dbf2bb69fd5871eb3f868f81
JonasFisk <Ldr_shadow@hotmail.com>
2018-11-16 02:02:17 +0100

    Added adduser method

--- src/main/java/HighScoreDB.java
+++ src/main/java/HighScoreDB.java
@@ -1,12 +1,27 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class HighScoreDB {
 
     public Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection("JDBC:mysql://localhost:3306/players", "grimestone", "abc123");
+        return DriverManager.getConnection("JDBC:mysql://localhost:3306/grimestone", "grimestone", "abc123");
+    }
+
+    public void addUserIfNew(String name) throws SQLException, ClassNotFoundException {
+        Statement statement = getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM players WHERE player_name = " + "'" + name + "'");
+        int rowCount = 0;
+        if(resultSet.last()){
+            rowCount = resultSet.getRow();
+            resultSet.beforeFirst();
+        }
+        if(rowCount == 0){
+            String query = "INSERT INTO players (player_name, wins, losses, draws) VALUES (?,0,0,0)";
+            PreparedStatement preparedStatement = getConnection().prepareStatement(query);
+            preparedStatement.setString(1, name);
+            preparedStatement.executeUpdate();
+        }
+        getConnection().close();
     }
 
 }
