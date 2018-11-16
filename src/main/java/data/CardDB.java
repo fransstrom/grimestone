7c29b8cc787d93b187c3c6bdf72edf8dbcf6d836
Mikael Andersson <mickepunktjohn@gmail.com>
2018-11-16 13:53:41 +0100

    Remade CardDB to only be a class to provide lists with cardconfigs (basically a Card template for CardFactory).

--- src/main/java/data/CardDB.java
+++ src/main/java/data/CardDB.java
@@ -1,43 +1,37 @@
-import cards.Card;
+package data;
+
 import cards.CreatureCard;
 import cards.MagicCard;
 import cards.SpecialCreatureCard;
 import cards.effects.HealPlayerEffect;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class CardDB {
 
-    private static final Random rand = new Random();
-    private static ArrayList<CreatureCard> creatureCards = new ArrayList<>();
-    private static ArrayList<SpecialCreatureCard> specialCreatureCards = new ArrayList<>();
-
-    private static ArrayList<MagicCard> magicCards = new ArrayList<>();
+    private static ArrayList<CreatureCard> creatureCardConfigs = new ArrayList<>();
     static {
-        magicCards.add(new MagicCard("Holy Fire", 3, new HealPlayerEffect(7)));
-        creatureCards.add(new CreatureCard("Mangry Hamster", 1, CreatureCard.Type.NEUTRAL, 1, 2, 1, 2));
-        creatureCards.add(new CreatureCard("Trippy Toad",1, CreatureCard.Type.GRASS, 1 , 1, 1, 3));
-        specialCreatureCards.add(new SpecialCreatureCard("Holy Dragon", 5, CreatureCard.Type.NEUTRAL, 3, 5, 5, 7, new HealPlayerEffect(5)));
+        creatureCardConfigs.add(new CreatureCard("Mangry Hamster", 1, CreatureCard.Type.NEUTRAL, 1, 2, 1, 2));
+        creatureCardConfigs.add(new CreatureCard("Trippy Toad",1, CreatureCard.Type.GRASS, 1 , 1, 1, 3));
+    }
+    private static ArrayList<SpecialCreatureCard> specialCreatureCardConfigs = new ArrayList<>();
+    static {
+        specialCreatureCardConfigs.add(new SpecialCreatureCard("Holy Dragon", 5, CreatureCard.Type.NEUTRAL, 3, 5, 5, 7, new HealPlayerEffect(5)));
+    }
+    private static ArrayList<MagicCard> magicCardConfigs = new ArrayList<>();
+    static {
+        magicCardConfigs.add(new MagicCard("Holy Fire", 3, new HealPlayerEffect(7)));
     }
 
-    public static Card getRandomCreatureCard(){
-        int bound = creatureCards.size();
-        int index = rand.nextInt(bound);
-        return creatureCards.get(index);
+    public static ArrayList<CreatureCard> getCreatureCardConfigs() {
+        return creatureCardConfigs;
     }
 
-    public static Card getRandomSpecialCreatureCard(){
-        int bound = specialCreatureCards.size();
-        int index = rand.nextInt(bound);
-        return specialCreatureCards.get(index);
+    public static ArrayList<SpecialCreatureCard> getSpecialCreatureCardConfigs() {
+        return specialCreatureCardConfigs;
     }
 
-    public static Card getRandomMagicCard(){
-        int bound = magicCards.size();
-        int index = rand.nextInt(bound);
-        return magicCards.get(index);
+    public static ArrayList<MagicCard> getMagicCardConfigs() {
+        return magicCardConfigs;
     }
-
-
 }
